36fddaf0617683e6a1e14dd7e1f5d7ae3d96c549
Sandip Pathe <113054078+sandip-pathe@users.noreply.github.com>
2024-01-01 03:47:25 +0530

    Craeted/Added some components from Internet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,21 @@
-import { Button, Image, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import * as ImagePicker from 'expo-image-picker'
-import ImageInput from '../../components/ImageInput';
-import ImageInputList from '../../components/ImageInputList';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-const App = () => {
-    const [imageUris, setImageUris] = useState([]);
-
-    const handleAdd = uri => {
-        setImageUris([...imageUris, uri])
-    }
-    const handleRemove = uri => {
-        setImageUris(imageUris.filter(imageUri => imageUri !== uri));
-    }
-
+const index = () => {
     return (
         <View style={styles.container}>
-            <ImageInputList
-                imageUris={imageUris}
-                onAddImage={handleAdd}
-                onRemoveImage={handleRemove}
-            />
+            <Text style={{ fontSize: 24, color: "#00ff00" }}>Sandip</Text>
         </View>
     )
 }
 
-export default App
+export default index
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 16,
-        alignContent: 'center',
-        justifyContent: 'center'
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
--- app/EventReports/ScreenTwo.tsx
+++ app/EventReports/ScreenTwo.tsx
@@ -2,72 +2,30 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import MyInputField from '../../components/MyInputField';
 import { defaultStyles } from '../../constants/Styles';
-import DocumentPicker, { DocumentPickerResponse } from 'react-native-document-picker';
-import ImagePicker, { ImagePickerResponse } from 'react-native-image-picker';
+import ImageInputList from '../../components/ImageInputList';
 
-interface ScreenTwoProps {
-    navigation: any; // Adjust the type based on your navigation library
-}
+type UriType = string;
 
-const ScreenTwo: React.FC<ScreenTwoProps> = ({ navigation }) => {
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
-    const [selectedDocument, setSelectedDocument] = useState<string | null>(null);
+const ScreenTwo: React.FC = () => {
+    const [imageUris, setImageUris] = useState<UriType[]>([]);
 
-    // const handleImagePicker = () => {
-    //     ImagePicker.showImagePicker({}, (response: ImagePickerResponse) => {
-    //         if (response.uri) {
-    //             setSelectedImage(response.uri);
-    //         }
-    //     });
-    // };
+    const handleAdd = (uri: UriType) => {
+        setImageUris([...imageUris, uri]);
+    }
 
-    // const handleDocumentPicker = async () => {
-    //     try {
-    //         const result: DocumentPickerResponse = await DocumentPicker.pick({
-    //             type: [DocumentPicker.types.allFiles],
-    //         });
+    const handleRemove = (uri: UriType) => {
+        setImageUris(imageUris.filter(imageUri => imageUri !== uri));
+    }
 
-    //         if (result.uri) {
-    //             setSelectedDocument(result.uri);
-    //         }
-    //     } catch (err) {
-    //         if (DocumentPicker.isCancel(err)) {
-    //             // User cancelled the document picker
-    //         } else {
-    //             console.error('Error picking document:', err);
-    //         }
-    //     }
-    // };
 
     return (
         <View style={styles.container}>
             <MyInputField label="Objectives Achieved" />
-
-            {/* Image Attachment */}
-            <TouchableOpacity style={defaultStyles.btn}>
-                <Text style={defaultStyles.btnText}>Attach Images</Text>
-            </TouchableOpacity>
-
-            {/* Document Attachment */}
-            <TouchableOpacity style={defaultStyles.btn}>
-                <Text style={defaultStyles.btnText}>Attach Financial Reports</Text>
-            </TouchableOpacity>
-
-            {/* Display Selected Image and Document (if any) */}
-            {selectedImage && (
-                <View>
-                    <Text>Selected Image:</Text>
-                    <Image source={{ uri: selectedImage }} style={{ width: 200, height: 200 }} />
-                </View>
-            )}
-
-            {selectedDocument && (
-                <View>
-                    <Text>Selected Document:</Text>
-                    <Text>{selectedDocument}</Text>
-                </View>
-            )}
-
+            <ImageInputList
+                imageUris={imageUris}
+                onAddImage={handleAdd}
+                onRemoveImage={handleRemove}
+            />
             <TouchableOpacity style={defaultStyles.btn}>
                 <Text style={defaultStyles.btnText}>Submit</Text>
             </TouchableOpacity>
